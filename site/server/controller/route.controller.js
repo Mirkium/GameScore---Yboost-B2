efd56a663c91fa2a1c402872dd62bd3c421061f1
Arkium69 <apetitfrerestudio999@gmail.com>
2026-02-25 10:46:55 +0100

    init

---
+++ site/server/controller/route.controller.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const path = require('path');
+
+// ============== Home & Search ==============
+exports.getHome = async (req, res) => {
+
+}
+
+exports.postSearch = async (req, res) => {
+
+}
+
+
+// ============== Connect & Auth ==============
+exports.getConnect = async (req, res) => {
+
+}
+
+exports.postLogin = async (req, res) => {
+
+}
+
+exports.postRegister = async (req, res) => {
+
+}
+
+
+// ============== Game, Eval & Comment ==============
+exports.getGame = async (req, res) => {
+
+}
+
+exports.postEval = async (req, res) => {
+
+}
+
+exports.postComment = async (req, res) => {
+
+}
+
+
+// ============== Profil, like and del ==============
+exports.getProfil = async (req, res) => {
+
+}
+
+exports.postLikeGame = async (req, res) => {
+
+}
+
+exports.postDelAccompt = async (req, res) => {
+
+}
